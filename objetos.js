65741f46556dfe37d2c3fcb1a19c97f52fc96c56
susmoreno <sus.alejandror@gmail.com>
2023-08-22 22:04:45 -0300

    Create objetos.js

---
+++ objetos.js
@@ -0,0 +1,24 @@
+const jesus = {
+    firstName: "Jesús",
+    lastName: "Moreno",
+    age: 28,
+
+    getFirstName: function(){
+        return this.firstName;
+    },
+
+    setFirstName: function (newFirstName){
+        this.firstName = newFirstName;
+    },
+
+    getLastName: function() {
+        return this.lastName;
+    },
+
+    getName: function(){
+        return this.getFirstName() + "" + this.getLastName();
+    }
+}
+
+jesus.setFirstName("Jesús Alejandro")
+console.log(jesus.getName());
